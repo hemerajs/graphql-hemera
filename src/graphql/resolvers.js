afc9cd7f58bbb6fb6c93452eb12e9fd7dd342c60
Dustin Deus <deusdustin@gmail.com>
2018-03-11 21:26:56 +0100

    upgrade to hemera 4

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,27 +1,33 @@
 const resolvers = hemera => ({
   Query: {
     getUserById(root, { id }) {
-      return hemera.act({
-        topic: 'user',
-        cmd: 'getUserById',
-        id
-      })
+      return hemera
+        .act({
+          topic: 'user',
+          cmd: 'getUserById',
+          id
+        })
+        .then(resp => resp.data)
     },
     getUserByEmail(root, { email }) {
-      return hemera.act({
-        topic: 'user',
-        cmd: 'getUserByEmail',
-        email
-      })
+      return hemera
+        .act({
+          topic: 'user',
+          cmd: 'getUserByEmail',
+          email
+        })
+        .then(resp => resp.data)
     }
   },
   Mutation: {
     createUser(root, args) {
-      return hemera.act({
-        topic: 'user',
-        cmd: 'createUser',
-        user: args
-      })
+      return hemera
+        .act({
+          topic: 'user',
+          cmd: 'createUser',
+          user: args
+        })
+        .then(resp => resp.data)
     }
   }
 })
